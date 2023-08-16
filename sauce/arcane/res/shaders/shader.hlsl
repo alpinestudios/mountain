28d0662fc68d2d2e4f953b467fff551c189aabc1
unknown <tom@randy.gg>
2023-08-16 19:03:26 +0700

    lighting system doneskis

--- sauce/arcane/res/shaders/shader.hlsl
+++ sauce/arcane/res/shaders/shader.hlsl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3cc948a158c4b2d568db5444592bab382a9dd7a12411cae33e7be2403eb6de4
-size 3031
+oid sha256:75a0189f9562ff1265f4adaa0a0284b6fe608c1bab927cb42ec8a737a3dd6aa6
+size 3795
--- sauce/arcane/res/shaders/vs.hlsl
+++ sauce/arcane/res/shaders/vs.hlsl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eca966d778ae354ab97b49dedb09a656e69211a9d82fe389e09586757e2550b3
+oid sha256:85a85b9e32d83f2db400034fd8eaf1187d9ea91a1795f072666c1a86f72c1624
 size 20924
