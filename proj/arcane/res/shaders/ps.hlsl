a369d13fee9758aaa8533dcf0e8aed9b7ae7a126
unknown <tom@randy.gg>
2023-08-30 09:43:02 +0700

    renamed .hlsl bytecode shader so it's not confusing

--- proj/arcane/res/shaders/ps.hlsl
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0a0c7084178b13c9c845972d8b493a50ee7e32393033d4fd0d25d88ccce051ee
-size 40752
--- proj/arcane/res/shaders/vs.hlsl
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d872ce340c8ab12e757233ed6b57fd1ef8f9d433cd73380a7a2dff396394e6e3
-size 20924
