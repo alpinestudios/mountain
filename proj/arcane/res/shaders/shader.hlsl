e3fa502c0189678ecfda910281c777cef0bf50d5
unknown <tom@randy.gg>
2023-08-31 09:19:48 +0700

    rendering refactor/cleanup

--- proj/arcane/res/shaders/shader.hlsl
+++ proj/arcane/res/shaders/shader.hlsl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e339fd11d4cea205c8f5d389568873e122063bf184c4045521eb3c24b0d5b08b
-size 3901
+oid sha256:5128652ed9a5d8c6e9e414634827ab313fe43309fba62ecb4a31b291b668f52e
+size 4527
