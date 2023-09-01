932fd91e993399c7c461001d1158ec0fc3ef20b3
unknown <tom@randy.gg>
2023-09-01 07:39:37 +0700

    disabled autopickup into hand

--- proj/arcane/res/shaders/shader.hlsl
+++ proj/arcane/res/shaders/shader.hlsl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5128652ed9a5d8c6e9e414634827ab313fe43309fba62ecb4a31b291b668f52e
-size 4527
+oid sha256:a748d58e9e90b04cc2c04a4f054a7bc32479da2b924631475c8b3c324521a6bc
+size 4598
